25f2eacb584bbba9fb8aaeb903c0fb5136a0fbe2
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-07-12 08:10:22 +0000

    Made the start page

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <button class="button">Start</button>
   </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+  width: 100%;
+  background-color: #f0f8ff;
+}
+
+.button {
+  padding: 0.75rem 1.5rem;
+  background-color: #0095ff;
+  color: white;
+  border: none;
+  border-radius: 6px;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(0.9);
+  }
+
+  &:focus {
+    filter: brightness(0.9);
+  }
+}
+</style>
