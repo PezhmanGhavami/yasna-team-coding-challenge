d27031a93be3557e23722e5e5d29bd81e7f47ea7
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-07-12 09:48:23 +0000

    Made the quiz store and a util function to handle importing the questions and shuffling the answer array

---
+++ stores/quiz.ts
@@ -0,0 +1,81 @@
+import { defineStore } from "pinia";
+
+import { shuffledQuizData } from "~/utils/shuffleQuizData";
+
+type Progress = boolean | null;
+
+export const useQuizStore = defineStore("quiz", () => {
+  const quizData = ref(shuffledQuizData());
+  const hasStarted = ref(false);
+  const isFinished = ref(false);
+  const index = ref(0);
+  const answerProgress = ref(Array<Progress>(quizData.value.length).fill(null));
+
+  const timerSeconds = 10;
+
+  const currentQuestion = computed(() => quizData.value[index.value]);
+
+  const quizResult = computed(
+    () =>
+      (answerProgress.value.reduce(
+        (acc, answer) => (answer ? acc + 1 : acc),
+        0
+      ) *
+        100) /
+      answerProgress.value.length
+  );
+
+  function reset() {
+    const newQuizData = shuffledQuizData();
+    quizData.value = newQuizData;
+    hasStarted.value = false;
+    isFinished.value = false;
+    index.value = 0;
+    answerProgress.value = Array<Progress>(newQuizData.length).fill(null);
+  }
+
+  function handleIndexUpdateAndNavigation() {
+    if (index.value === quizData.value.length - 1) {
+      hasStarted.value = false;
+      isFinished.value = true;
+      return navigateTo("/result");
+    }
+    index.value++;
+    navigateTo(`/question/${index.value + 1}`);
+  }
+
+  function answerQuestion(answerIsCorrect: boolean) {
+    answerProgress.value[index.value] = answerIsCorrect;
+    handleIndexUpdateAndNavigation();
+  }
+
+  function markQuestionAsWrong() {
+    answerProgress.value[index.value] = false;
+    handleIndexUpdateAndNavigation();
+  }
+
+  function startTheQuiz() {
+    hasStarted.value = true;
+    isFinished.value = false;
+    navigateTo(`/question/1`);
+  }
+
+  function restartTheQuiz() {
+    reset();
+    startTheQuiz();
+  }
+
+  return {
+    quizData,
+    hasStarted,
+    isFinished,
+    quizResult,
+    timerSeconds,
+    answerProgress,
+    currentQuestion,
+    startTheQuiz,
+    restartTheQuiz,
+    answerQuestion,
+    markQuestionAsWrong,
+  };
+});
---
+++ utils/shuffleQuizData.ts
@@ -0,0 +1,22 @@
+import quizData from "~/assets/data.json";
+
+function getShuffledArray(initialArray: any[]) {
+  const localArray = [...initialArray];
+
+  for (let index = localArray.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [localArray[index], localArray[randomIndex]] = [
+      localArray[randomIndex],
+      localArray[index],
+    ];
+  }
+
+  return localArray;
+}
+
+export function shuffledQuizData() {
+  return quizData.map((question) => ({
+    question_text: question.question_text,
+    answers: getShuffledArray(question.answers),
+  }));
+}
